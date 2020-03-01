f9f4b7db5d747e48a6fb50a5f2e87b28bde28327
Ricky <info@rickywallace.co.uk>
2020-03-01 11:11:30 +0000

    Installed new css pre and post css plugins and autoprefixer. Updated webpack.config file

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// We do not need to npm download 'path' as this is part of the node library
+const path = require('path');
+
+const postCSSPlugins = [
+    require('postcss-simple-vars'),
+    require('postcss-nested'),
+    require('autoprefixer')
+]
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js', 
+        // by using below path,, we make sure regardless of what operating system we use, that we have an absolute path to the correct directory
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch: true,
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+            }
+        ]
+    }
+}
