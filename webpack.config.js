58e3d85dfb3424bd112f1aeb641179c3169b9f92
Ricky <info@rickywallace.co.uk>
2020-03-01 19:43:59 +0000

    Added mixins through webpack and configured

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 
 const postCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
@@ -15,8 +16,16 @@ module.exports = {
         // by using below path,, we make sure regardless of what operating system we use, that we have an absolute path to the correct directory
         path: path.resolve(__dirname, 'app')
     },
+    devServer: {
+        before: function (app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host: '0.0.0.0'
+    },
     mode: 'development',
-    watch: true,
     module: {
         rules: [
             {
