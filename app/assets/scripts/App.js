a7b7851f429ac3e4b3e7c9c96578c1c90f16bfaa
Ricky <info@rickywallace.co.uk>
2020-03-22 11:29:14 +0000

    added minified css to our build

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -4,7 +4,7 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
-
+alert('testing!!!!!!!');
 new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
+const currentTask = process.env.npm_lifecycle_event;
+
 // We do not need to npm download 'path' as this is part of the node library
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -9,14 +13,28 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
-module.exports = {
+let cssConfig = {
+    test: /\.css$/i,
+    use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+};
+
+let config = {
     entry: './app/assets/scripts/App.js',
-    output: {
+    module: {
+        rules: [
+            cssConfig
+        ]
+    }
+};
+
+if (currentTask == 'dev') {
+    cssConfig.use.unshift('style-loader');
+    config.output = {
         filename: 'bundled.js', 
         // by using below path,, we make sure regardless of what operating system we use, that we have an absolute path to the correct directory
         path: path.resolve(__dirname, 'app')
     },
-    devServer: {
+    config.devServer = {
         before: function (app, server) {
             server._watch('./app/**/*.html')
         },
@@ -25,13 +43,24 @@ module.exports = {
         port: 3000,
         host: '0.0.0.0'
     },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-            }
-        ]
-    }
+    config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+    cssConfig.use.unshift(MiniCssExtractPlugin.loader);
+    postCSSPlugins.push(require('cssnano'));
+    config.output = {
+        filename: '[name].[chunkhash].js', 
+        chunkFilename: '[name].[chunkhash].js',
+        // by using below path,, we make sure regardless of what operating system we use, that we have an absolute path to the correct directory
+        path: path.resolve(__dirname, 'dist')
+    },
+    config.mode = 'production'
+    config.optimization = {
+        splitChunks: {chunks: 'all'}
+    }
+
+    config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'})];
+}
+
+module.exports = config;
