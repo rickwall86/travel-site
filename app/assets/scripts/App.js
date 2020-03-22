229bbb306fd39f62257a0d525c1eedbdfd713da2
Ricky <info@rickywallace.co.uk>
2020-03-22 19:47:53 +0000

    Completed our build process configuration

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,6 @@ import 'lazysizes';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-
-alert('testing!!!!!!!');
 new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -13,13 +15,32 @@ const postCSSPlugins = [
     require('autoprefixer')
 ]
 
+class RunAfterCompile {
+    apply(compiler) {
+        compiler.hooks.done.tap('Copy images', function() {
+            fse.copySync('./app/assets/images', './dist/assets/images')
+        })
+    }
+}
+
 let cssConfig = {
     test: /\.css$/i,
     use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
 };
 
+let pages = fse.readdirSync('./app').filter(function(file) {
+    return file.endsWith('.html');
+}).map(function(page) {
+    return new HtmlWebpackPlugin({
+        filename: page,
+        template: `./app/${page}`
+    })
+})
+
+// universal config used for both build & dev
 let config = {
     entry: './app/assets/scripts/App.js',
+    plugins: pages,
     module: {
         rules: [
             cssConfig
@@ -27,6 +48,7 @@ let config = {
     }
 };
 
+// used only for dev 
 if (currentTask == 'dev') {
     cssConfig.use.unshift('style-loader');
     config.output = {
@@ -46,7 +68,19 @@ if (currentTask == 'dev') {
     config.mode = 'development'
 }
 
+// used only for build
 if (currentTask == 'build') {
+    config.module.rules.push({
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env']
+            }
+        }
+    })
+
     cssConfig.use.unshift(MiniCssExtractPlugin.loader);
     postCSSPlugins.push(require('cssnano'));
     config.output = {
@@ -60,7 +94,11 @@ if (currentTask == 'build') {
         splitChunks: {chunks: 'all'}
     }
 
-    config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'})];
+    config.plugins.push(
+        new CleanWebpackPlugin(), 
+        new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'}),
+        new RunAfterCompile()
+    )
 }
 
 module.exports = config;
