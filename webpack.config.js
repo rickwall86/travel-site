04898a8b2d61d7072efc9a13f5d98c89e42b3b15
Ricky <info@rickywallace.co.uk>
2020-04-01 11:28:39 +0100

    Changed webpack config to build into dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const postCSSPlugins = [
 class RunAfterCompile {
   apply(compiler) {
     compiler.hooks.done.tap('Copy images', function() {
-      fse.copySync('./app/assets/images', './docs/assets/images')
+      fse.copySync('./app/assets/images', './dist/assets/images')
     })
   }
 }
@@ -81,7 +81,7 @@ if (currentTask == 'build') {
   config.output = {
     filename: '[name].[chunkhash].js',
     chunkFilename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'docs')
+    path: path.resolve(__dirname, 'dist')
   }
   config.mode = 'production'
   config.optimization = {
